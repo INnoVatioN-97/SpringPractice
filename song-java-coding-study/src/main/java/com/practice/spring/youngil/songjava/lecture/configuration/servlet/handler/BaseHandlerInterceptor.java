ec0f04944cbee90a63d997d242504bba9538aac0
youngil Ko <koyoungil97@gmail.com>
2022-01-02 19:06:00 +0900

    log4jdbc, logback 사용 실습

---
+++ song-java-coding-study/src/main/java/com/practice/spring/youngil/songjava/lecture/configuration/servlet/handler/BaseHandlerInterceptor.java
@@ -0,0 +1,31 @@
+package com.practice.spring.youngil.songjava.lecture.configuration.servlet.handler;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 강의상 HandlerInterceptorAdapter 를 상속받도록 되었으나,
+ * Deprecated 되어 직접 인터페이스를 구현하는 방식으로 변경됨
+ */
+public class BaseHandlerInterceptor implements HandlerInterceptor {
+
+    Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        logger.info("preHandle requestURI : {}", request.getRequestURI());
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        logger.info("postHandle requestURI : {}", request.getRequestURI());
+    }
+}
