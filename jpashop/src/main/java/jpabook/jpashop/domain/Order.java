9b4b5ba39cdd29081124a0c964dce8b647bca08f
“INnoVatioN-97” <koyoungil97@gmail.com>
2022-01-20 11:05:53 +0900

    실전 예제 - 요구사항 분석과 기본 매핑 수강

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,61 @@
+package jpabook.jpashop.domain;
+
+import jpabook.jpashop.domain.OrderStatus;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+/**
+ * Member - Order 1:N 테이블
+ */
+@Entity
+@Table(name = "ORDERS")
+public class Order {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "ORDER_ID")
+    private Long id;
+
+    // 누가 주문했는지 알기 위함.
+    @Column(name = "MEMBER_ID")
+    private Long memberId;
+
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+}
