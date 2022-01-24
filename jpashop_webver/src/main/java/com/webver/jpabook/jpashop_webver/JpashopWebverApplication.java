38d01c596acbde7151f645ba50f9d803c72e6c46
“INnoVatioN-97” <koyoungil97@gmail.com>
2022-01-24 21:46:53 +0900

    initial commit

---
+++ jpashop_webver/src/main/java/com/webver/jpabook/jpashop_webver/JpashopWebverApplication.java
@@ -0,0 +1,13 @@
+package com.webver.jpabook.jpashop_webver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpashopWebverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpashopWebverApplication.class, args);
+	}
+
+}
---
+++ jpashop_webver/src/test/java/com/webver/jpabook/jpashop_webver/JpashopWebverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webver.jpabook.jpashop_webver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpashopWebverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
