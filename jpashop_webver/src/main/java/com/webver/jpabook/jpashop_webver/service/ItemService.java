555f389c3c6572d3a3536723ae661903f8a12da9
“INnoVatioN-97” <koyoungil97@gmail.com>
2022-02-02 14:04:27 +0900

    변경감지와 병합(merge)

--- jpashop_webver/src/main/java/com/webver/jpabook/jpashop_webver/service/ItemService.java
+++ jpashop_webver/src/main/java/com/webver/jpabook/jpashop_webver/service/ItemService.java
@@ -1,5 +1,6 @@
 package com.webver.jpabook.jpashop_webver.service;
 
+import com.webver.jpabook.jpashop_webver.domain.item.Book;
 import com.webver.jpabook.jpashop_webver.domain.item.Item;
 import com.webver.jpabook.jpashop_webver.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -20,6 +21,15 @@ public class ItemService {
         itemRepository.save(item);
     }
 
+    // 보다 중간에 현타와서 놓침. 변경감지와 병합(merge) 7분 30초부터 듣기
+    public void updateItem(Long itemId, String name, int price, int stockQuantity) {
+        Item item = itemRepository.findOne(itemId);
+        item.setName(name);
+        item.setPrice(price);
+        item.setStockQuantity(stockQuantity);
+        // 영속상태의 엔티티 이므로 em.persist, merge 이런걸 호출할 필요가 없다. 알아서 flush() 되고 수정됨.
+    }
+
     public List<Item> findItems(){
         return itemRepository.findAll();
     }
@@ -27,4 +37,5 @@ public class ItemService {
     public Item findOne(Long itemId) {
         return itemRepository.findOne(itemId);
     }
+
 }
